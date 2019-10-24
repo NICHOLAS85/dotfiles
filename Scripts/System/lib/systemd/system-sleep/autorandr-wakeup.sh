91abc39f30157102534083b9d2caabb8eec4aa7f
NICHOLAS85 <15698885+NICHOLAS85@users.noreply.github.com>
2019-10-23 23:38:50 -0400

    Change autorandr-wakeup perms and text, update clight.conf

--- Scripts/System/lib/systemd/system-sleep/autorandr-wakeup.sh
+++ Scripts/System/lib/systemd/system-sleep/autorandr-wakeup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case $1 in
   post)
-    /usr/bin/autorandr --batch --change && echo "Autorandr: Batch changed"
+    /usr/bin/autorandr --batch --change && echo "Autorandr: Loaded first detected profile on all active sessions"
     ;;
 esac
