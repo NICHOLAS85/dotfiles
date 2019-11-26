150c6506f3987de18f3944521f339a61bb60d06a
NICHOLAS85 <15698885+NICHOLAS85@users.noreply.github.com>
2019-11-25 21:36:03 -0500

    Update refindhibernate.sh to use a specific timeout file instead of moving the whole theme file

--- Scripts/System/lib/systemd/system-sleep/refindhibernate.sh
+++ Scripts/System/lib/systemd/system-sleep/refindhibernate.sh
@@ -4,13 +4,13 @@ if [ "$2" = "hibernate" ]; then
     case "$1" in
         pre)
             echo "refindhibernate.sh: Setting timeout to -1"
-            sed 's/timeout.*/timeout -1/g' /boot/efi/EFI/refind/themes/refind-ambience/theme.conf > /boot/efi/EFI/refind/themes/refind-ambience/theme.conf~
-            mv /boot/efi/EFI/refind/themes/refind-ambience/theme.conf~ /boot/efi/EFI/refind/themes/refind-ambience/theme.conf
+            sed 's/timeout.*/timeout -1/g' /boot/efi/EFI/refind/timeout.conf > /boot/efi/EFI/refind/timeout.conf~
+            mv /boot/efi/EFI/refind/timeout.conf~ /boot/efi/EFI/refind/timeout.conf
         ;;
         post)
             echo "refindhibernate.sh: Setting timeout to 5"
-            sed 's/timeout.*/timeout 5/g' /boot/efi/EFI/refind/themes/refind-ambience/theme.conf > /boot/efi/EFI/refind/themes/refind-ambience/theme.conf~
-            mv /boot/efi/EFI/refind/themes/refind-ambience/theme.conf~ /boot/efi/EFI/refind/themes/refind-ambience/theme.conf
+            sed 's/timeout.*/timeout 5/g' /boot/efi/EFI/refind/timeout.conf > /boot/efi/EFI/refind/timeout.conf~
+            mv /boot/efi/EFI/refind/timeout.conf~ /boot/efi/EFI/refind/timeout.conf
             ;;
     esac
 fi
