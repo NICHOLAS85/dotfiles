fda5fcec87119084095bd805458eaedf2e80ceb3
NICHOLAS85 <15698885+NICHOLAS85@users.noreply.github.com>
2020-02-25 16:34:19 -0500

    Use minwaita instead of breeze for firefox

--- .mozilla/firefox/m6ar4kzt.default-beta-back-ovfs/chrome/user.js
+++ .mozilla/firefox/m6ar4kzt.default-beta-back-ovfs/chrome/user.js
@@ -18,4 +18,4 @@ user_pref("gfx.webrender.all", true);
 user_pref("layers.acceleration.force-enabled", true);
 
 /* Fix dark widgets with dark theme */
-user_pref("widget.content.gtk-theme-override", "Breeze:light");
+user_pref("widget.content.gtk-theme-override", "Minwaita:light");
