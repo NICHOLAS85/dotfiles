974c7acf15f375f8cc72dd3a7a8815681818f257
NICHOLAS85 <15698885+NICHOLAS85@users.noreply.github.com>
2020-05-22 00:03:12 -0400

    Update Clight, add new modules

--- .local/share/clight/inhibit_bl3.c
+++ .local/share/clight/inhibit_bl3.c
@@ -9,14 +9,14 @@
  * Small example custom module for Clight.
  *
  * It just hooks on INHIBIT state updates and:
- * -> when entering INHIBIT state (eg: start watching a movie), disables automatic calibration and switch off kbd backlight
- * -> when leaving INHIBIT state, triggers a new backlight calibration and re-enables automatic calibration
+ * -> when entering INHIBIT state (eg: start watching a movie), disables automatic calibration, switch off kbd backlight, pause night color
+ * -> when leaving INHIBIT state, triggers a new backlight calibration and re-enables automatic calibration, unpause night color
  **/
 
 /*
- * Rename to: inhibit_bl_mod.c
+ * Rename to: inhibit_bl3.c
  *
- * Build with: gcc -shared -fPIC inhibit_bl_mod.c -o inhibit_bl_mod -Wno-unused
+ * Build with: gcc -shared -fPIC inhibit_bl3.c -o inhibit_bl3 -Wno-unused
  *
  * Place inhibit_bl_mod in: $HOME/.local/share/clight/modules.d/ OR, globally, in /usr/share/clight/modules.d/
  */
@@ -26,11 +26,16 @@ CLIGHT_MODULE("INHIBIT_BL");
 DECLARE_MSG(capture_req, CAPTURE_REQ);
 DECLARE_MSG(calib_req, NO_AUTOCALIB_REQ);
 DECLARE_MSG(kbd_bl_req, KBD_BL_REQ);
+DECLARE_MSG(temp_req, TEMP_REQ);
 
 static void init(void) {
     capture_req.capture.reset_timer = false; // avoid resetting clight internal BACKLIGHT timer
     kbd_bl_req.bl.new = 0.0;
-
+    temp_req.temp.smooth = 1;
+    //temp_req.temp.step = 50;
+    //temp_req.temp.timeout = 100;
+    temp_req.temp.daytime = NIGHT;
+    
     /* Subscribe to inhibit state */
     M_SUB(INHIBIT_UPD);
 }
@@ -41,16 +46,16 @@ static void receive(const msg_t *msg, const void *userdata) {
         inhibit_upd *up = (inhibit_upd *)MSG_DATA();
         calib_req.nocalib.new = up->new;
         if (up->new) {
-            M_PUB(&calib_req);                // stop backlight autocalibration
-            M_PUB(&kbd_bl_req);               // set 0% kbd backlight
-            system("killall -USR1 redshift"); // Toggle redshift if active
-            INFO("Pausing autocalibration and redshift.\n");
+            INFO("Pausing autocalibration and night color.\n");
+            M_PUB(&kbd_bl_req);  // set 0% kbd backlight
+            temp_req.temp.new = 6500;
         } else {
-            INFO("Doing a quick backlight calibration and unpausing redshift.\n");
-            system("killall -USR1 redshift"); // toggle redshift if active
-            M_PUB(&calib_req);                // resume backlight autocalibration
-            M_PUB(&capture_req);              // ask for a quick calibration
+            INFO("Doing a quick backlight calibration and unpausing night color.\n");
+            M_PUB(&capture_req); // ask for a quick calibration
+            temp_req.temp.new = 4200;
         }
+        M_PUB(&temp_req);
+        M_PUB(&calib_req); // stop or start backlight autocalibration
         break;
     }
     default:
