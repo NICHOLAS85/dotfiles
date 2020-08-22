6c5483cac97f65f161f98aba99413c1476563705
NICHOLAS85 <15698885+NICHOLAS85@users.noreply.github.com>
2020-08-22 00:05:14 -0400

    Update materialfox and settings

--- .mozilla/firefox/sa2yeb0m.default-beta-back-ovfs/user.js
+++ .mozilla/firefox/sa2yeb0m.default-beta-back-ovfs/user.js
@@ -3,7 +3,7 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
 
 /* Move disk cache to RAM */
-user_pref("browser.cache.disk.parent_directory", "/run/user/1000/firefox")
+user_pref("browser.cache.disk.parent_directory", "/run/user/1000/firefox");
 
 /* Touchscreen firefox stuff */
 user_pref("browser.gesture.pinch.in", "");
@@ -14,7 +14,7 @@ user_pref("ui.touch.radius.enabled", true);
 
 /* Faster firefox? */
 user_pref("gfx.canvas.azure.accelerated", true);
-user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.all", false);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("browser.tabs.remote.force-enable", true);
 
@@ -31,3 +31,7 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("media.ffvpx.enabled", false);
 user_pref("media.ffmpeg.vaapi-drm-display.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
+user_preg("widget.wayland-dmabuf-vaapi.enabled", true);
+
+/* Allow smooth zooming */
+user_pref("apz.allow_zooming", true);
